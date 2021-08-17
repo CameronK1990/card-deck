91f465612bfa0ec3e6873ddc225174a2484a65dd
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-17 13:50:44 -0700

    ADD buttons WIP UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 
+const numAr = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+
+const suitAr = ["spades", "hearts", "clubs", "diamonds"];
+
+
+// UI logic
+(document).ready(function(){
+ ("button#deck").submit(function(event){
+    event.preventDefault();
+    numAr.forEach(function(number){
+      number.concat("of diamonds")
+    });
+    console.log(numAr);
+  });
+});
